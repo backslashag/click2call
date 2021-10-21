33f9d86e887fe3ed90864e416eca389ec0ad8cf5
Mischa Sameli <m.sameli@backslash.ch>
2021-10-21 07:43:42 +0200

    v 1.1, changed to nodes instead of plain text
    replacing is now done in text nodes only, not in plain html code

--- click2call.js
+++ click2call.js
@@ -1,7 +1,8 @@
 // click2call, activates phone number on smartphones
+// changelog: replacing now done in text nodes only, not in plain html code
 
 var phonenumber = {
-  defaults : {
+	defaults : {
 		'country': "ch",
 		'landwahl': "+41",
 		'linktitle': "",
@@ -10,129 +11,166 @@ var phonenumber = {
 		'excluderaw': "",
 		'excludeint': "",
 		'countryDefinitions' : ""
-
 	},
-	init : function (config) {
-		var prop;
-		if (config) {
-			for (prop in config) {
-				phonenumber.defaults[prop] = config[prop];
-			}
-		}
-		phonenumber.defaults['auto'] = true;
-		if(phonenumber.defaults['regexp']!=''){
-			phonenumber.defaults['auto']=false;
-			}
-		if(phonenumber.defaults['countryDefinitions']!=''){
-			var cd;
-			if (phonenumber.defaults['countryDefinitions']) {
-				for (cd in phonenumber.defaults['countryDefinitions']) {
-					phonenumber.countryDefinitions[cd] = phonenumber.defaults['countryDefinitions'][cd];
-			}
-		}
-			}
-		phonenumber.defaults['excluderaw'] = phonenumber.defaults['excluderaw'].split(',');
-		phonenumber.defaults['excludeint'] = phonenumber.defaults['excludeint'].split(',');
-		phonenumber.defaults['country'] = phonenumber.defaults['country'].split(',');
-
-		phonenumber._linkstring = "";
-		if(phonenumber.defaults.linktitle!="" ){
-			phonenumber._linkstring += "title='"+phonenumber.defaults.linktitle+"'";
-			}
-		if(phonenumber.defaults.linkclass!=""){
-			phonenumber._linkstring += "class='"+phonenumber.defaults.linkclass+"'";
-			}
-		return this;
-	},
-
-	countryDefinitions : {
-		'ch' : {'regex': [/(\+[0-9]{2}(\s|-|'|\+|\/))?(\(?0\)(\s)?)?[1-9]{2}(\s|-|'|\+|\/)?[0-9]{3}(\s|-|'|\+|\/)?[0-9]{2}(\s|-|'|\+|\/)?[0-9]{2}/g, /(\+[0-9]{2}(\s|-|'|\+|\/))?(\(?0\)?)?8[0-9]{2}(\s|-|'|\+|\/)?[0-9]{3}(\s|-|'|\+|\/)?[0-9]{3}/g ],
-		'landwahl': '+41'
-		}
-	},
-	_replaceNumbers : function (text, result, landvorwahl) {
-		var vorwahllength =landvorwahl.length;
-		for(var i = 0; i < result.length; i++){
-			var str = result[i];
-			if ( phonenumber.defaults.excluderaw.indexOf(str) < 0 ){
-				// delete whitespaces
-				var phoneNumber = str.replace(/\(0\)/g,'');
-				//replace all instead of
-				phoneNumber = phoneNumber.replace(/[^0-9\+]/g, '');
-
-				if(phoneNumber.substr(0,1) == "0"){
-					phoneNumber = landvorwahl + phoneNumber.substr(1, phoneNumber.length);
-					}
-
-				// if there are calling codes
-				if(vorwahllength){
-					// if there isn't a "plus"
-					if(phoneNumber.substr(0, 1) != "+"){
-						phoneNumber = landvorwahl + phoneNumber;
-						}
-					}
-
-				if ( phonenumber.defaults.excludeint.indexOf(phoneNumber) < 0 ){
-					//creating a tag
-					var newText = "<a href=\"tel:"+phoneNumber+"\" "+phonenumber._linkstring+" >"+str+"</a>";
-					text = text.replace(str, newText);
-					}
+		init : function (config) {
+			var prop;
+			if (config) {
+				for (prop in config) {
+					phonenumber.defaults[prop] = config[prop];
 				}
 			}
-		return text;
+			phonenumber.defaults['auto'] = true;
+			if(phonenumber.defaults['regexp']!==''){
+				phonenumber.defaults['auto']=false;
+				}
+			if(phonenumber.defaults['countryDefinitions']!==''){
+				var cd;
+				if (phonenumber.defaults['countryDefinitions']) {
+					for (cd in phonenumber.defaults['countryDefinitions']) {
+						phonenumber.countryDefinitions[cd] = phonenumber.defaults['countryDefinitions'][cd];
+				}
+			}
+				}
+			phonenumber.defaults['excluderaw'] = phonenumber.defaults['excluderaw'].split(',');
+			phonenumber.defaults['excludeint'] = phonenumber.defaults['excludeint'].split(',');
+			phonenumber.defaults['country'] = phonenumber.defaults['country'].split(',');
+
+			phonenumber._linkstring = "";
+			if(phonenumber.defaults.linktitle!=="" ){
+				phonenumber._linkstring += "title='"+phonenumber.defaults.linktitle+"'";
+				}
+			if(phonenumber.defaults.linkclass!==""){
+				phonenumber._linkstring += "class='"+phonenumber.defaults.linkclass+"'";
+				}
+			return this;
 		},
 
-	activate : function (){
-
-		var element = document.getElementsByTagName("body")[0];
-		var text = element.innerHTML;
-		if(!phonenumber.defaults['auto']){
-			//simple method with just a user defined regex
-			var test = phonenumber.defaults['regexp'];
-			var result = text.match(test);
-			if(result){
-
-				var landvorwahl = phonenumber.countryDefinitions['landwahl'];
-				//loop result;
-				text = phonenumber._replaceNumbers(text, result, landvorwahl);
-
-				}
+		countryDefinitions : {
+			'ch' : {
+				'regex': [
+					/(\+[0-9]{2}(\s|-|'|\+|\/))?(\(?0\)(\s)?)?[1-9]{2}(\s|-|'|\+|\/)?[0-9]{3}(\s|-|'|\+|\/)?[0-9]{2}(\s|-|'|\+|\/)?[0-9]{2}/g,
+					/(\+[0-9]{2}(\s|-|'|\+|\/))?(\(?0\)?)?8[0-9]{2}(\s|-|'|\+|\/)?[0-9]{3}(\s|-|'|\+|\/)?[0-9]{3}/g
+				],
+				'landwahl': '+41'
 			}
-		else{
-			//complex method: use a structure with country-defintions
-			for(var c=0; c<phonenumber.defaults['country'].length; c++){
-				if( typeof phonenumber.countryDefinitions[phonenumber.defaults.country[c]] != 'undefined' ){
-					for(var r=0; r<phonenumber.countryDefinitions[phonenumber.defaults.country[c]].regex.length; r++){
-						var test = phonenumber.countryDefinitions[phonenumber.defaults.country[c]].regex[r];
-						var result = text.match(test);
-						if(result){
-							// length calling code
-							var landvorwahl = phonenumber.countryDefinitions[phonenumber.defaults.country[c]].landwahl;
-							//loop result;
-							text = phonenumber._replaceNumbers(text, result, landvorwahl);
-							}
+		},
 
+		_insertAnchor: function(textnode, target, href, text) {
+			var txt = textnode.node.nodeValue;
+			var start = txt.indexOf(target);
+			var end = start + target.length;
+			
+			// reduce textnode text
+			textnode.node.nodeValue = txt.substring(0, start);
+			
+			// insert anchor
+			var anchor = document.createElement("a");
+			anchor.innerHTML = text;
+			anchor.href = href;
+			anchor.title = phonenumber.defaults.linktitle;
+			anchor.className = phonenumber.defaults.linkclass;
+			textnode.parent.insertBefore(anchor, textnode.node.nextSibling);
+			
+			// insert new text node with remaining text
+			var txt2 = document.createTextNode(txt.substring(end));
+			textnode.parent.insertBefore(txt2, anchor.nextSibling);
+			textnode.node = txt2;
+		},
+		
+		_processNode: function(textnode, hits, landvorwahl) {
+			for (var i = 0; i < hits.length; i++)
+				var str = hits[i];
+				if (str && phonenumber.defaults.excluderaw.indexOf(str) < 0 ){
+					// delete whitespaces
+					var phoneNumber = str.replace(/\(0\)/g,'');
+					//replace all instead of
+					phoneNumber = phoneNumber.replace(/[^0-9\+]/g, '');
+
+					if(phoneNumber.substr(0,1) == "0"){
+						phoneNumber = landvorwahl + phoneNumber.substr(1, phoneNumber.length);
+					}
+
+					// if there are calling codes
+					if(landvorwahl.length){
+						// if there isn't a "plus"
+						if(phoneNumber.substr(0, 1) != "+"){
+							phoneNumber = landvorwahl + phoneNumber;
 						}
 					}
+
+					if ( phonenumber.defaults.excludeint.indexOf(phoneNumber) < 0 ){
+						phonenumber._insertAnchor(textnode, str, "tel:"+phoneNumber, str);
+					}
 				}
-
+		},
+		
+		_getChildTextNodes: function(node) {
+			var nodes = [];
+			for (var i = 0; i < node.childNodes.length; i++) {
+				var child = node.childNodes[i];
+				if (child.nodeName.toLowerCase() === "script") {
+					continue;
+				} else if (child.nodeType === 3) {
+					nodes.push({node: child, parent: node});
+				} else if (child.nodeType === 1) {
+					nodes = nodes.concat(phonenumber._getChildTextNodes(child));
+				}
 			}
-			// write back
-			element.innerHTML = text;
-		}
-};
+			return nodes;
+		},
 
-if (!Array.prototype.indexOf) {
-  Array.prototype.indexOf = function (obj, fromIndex) {
-    if (fromIndex == null) {
-        fromIndex = 0;
-    } else if (fromIndex < 0) {
-        fromIndex = Math.max(0, this.length + fromIndex);
-    }
-    for (var i = fromIndex, j = this.length; i < j; i++) {
-        if (this[i] === obj)
-            return i;
-    }
-    return -1;
-  };
+		activate : function (){
+			var textnodes = phonenumber._getChildTextNodes(document.body);
+			for (var i = 0; i < textnodes.length; i++) {
+				
+				var textnode = textnodes[i];
+				
+				if(!phonenumber.defaults['auto']){
+					//simple method with just a user defined regex
+					var test = phonenumber.defaults['regexp'];
+					var result = textnode.node.nodeValue.match(test);
+					if(result){
+		
+						var landvorwahl = phonenumber.countryDefinitions['landwahl'];
+
+						phonenumber._processNode(textnode, result, landvorwahl);
+		
+					}
+				} else{
+					//complex method: use a structure with country-defintions
+					for(var c=0; c<phonenumber.defaults['country'].length; c++){
+						if( typeof phonenumber.countryDefinitions[phonenumber.defaults.country[c]] != 'undefined' ){
+							for(var r=0; r<phonenumber.countryDefinitions[phonenumber.defaults.country[c]].regex.length; r++){
+								var test = phonenumber.countryDefinitions[phonenumber.defaults.country[c]].regex[r];
+								var result = textnode.node.nodeValue.match(test);
+								if(result){
+									// length calling code
+									var landvorwahl = phonenumber.countryDefinitions[phonenumber.defaults.country[c]].landwahl;
+
+									phonenumber._processNode(textnode, result, landvorwahl);
+									}
+		
+								}
+							}
+					}
+				}
+				
+			}
+		}
+	};
+
+	if (!Array.prototype.indexOf) {
+	Array.prototype.indexOf = function (obj, fromIndex) {
+		if (fromIndex == null) {
+			fromIndex = 0;
+		} else if (fromIndex < 0) {
+			fromIndex = Math.max(0, this.length + fromIndex);
+		}
+		for (var i = fromIndex, j = this.length; i < j; i++) {
+			if (this[i] === obj)
+				return i;
+		}
+		return -1;
+	};
 }
+  
